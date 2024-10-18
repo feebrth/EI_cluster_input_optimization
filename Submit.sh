bed2df72ede2ea4e87e1db8c25346ec475d90839
fbierth <felicitasbierth@gmail.com>
2024-10-18 13:17:49 +0200

    Submit file

---
+++ Submit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=study
+#SBATCH --output=logs/study%A_%a.out
+#SBATCH --error=logs/study_job_%A_%a.err
+#SBATCH --array=0-9
+#SBATCH --partition=all
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=2G
+#SBATCH --time=02:00:00
+
+# Load necessary modules
+module load python3/anaconda3-2024.06-py3.12
+
+# Activate conda environment
+conda activate Nest3
+
+
+# Run the Python script
+python RandomSearch_Single.py
