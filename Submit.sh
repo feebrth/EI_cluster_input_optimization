27695cbdffb9cb4fc40f1cba57b0dc9221695e0b
fbierth <felicitasbierth@gmail.com>
2024-10-22 11:40:44 +0200

    Optuna 8 stimuli Submit

--- Submit.sh
+++ Submit.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=3G
-#SBATCH --time=05:00:00
+#SBATCH --time=04:59:00
 
 # Load necessary modules
 module load python3/anaconda3-2024.06-py3.12
