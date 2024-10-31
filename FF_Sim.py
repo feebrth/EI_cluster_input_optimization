2fb69eee232b5c6e2e5920b068c5c449ea413484
fbierth <felicitasbierth@gmail.com>
2024-10-31 10:55:03 +0100

    FF_Sim and Submit.sh

--- FF_Sim.py
+++ FF_Sim.py
@@ -249,11 +249,11 @@ def simulate_model(experimental_trials, direction_range, stim_kernel, kernel_ste
 # Run the simulation
 if __name__ == "__main__":
     simulate_model(
-        experimental_trials= 60,  # number of trials
+        experimental_trials= 5,  # number of trials
         direction_range=[0, 1, 2],  # direction range
         stim_kernel=np.array([0.4000331873740433, 0.4721548317794393, 0.2862214498669786, 0.26492405767086835,
                               0.3643699894838586, 0.4197222604716706, 0.26557664779830137, 0.058329996453807736,
                               0.0, 0.42674567043483264, 1.0, 1.0]),  # Stimulus-Kernel
-        kernel_step= 2000/12,  # kernel step 167 ms pro stimulus
+        kernel_step= 2000//12,  # kernel step 167 ms pro stimulus
         plot=True
     )
--- Submit.sh
+++ Submit.sh
@@ -18,4 +18,4 @@ conda activate Nest3
 
 
 # Run the Python script
-python Optuna_12_Stimuli.py
+python Optuna_24_Stimuli.py
