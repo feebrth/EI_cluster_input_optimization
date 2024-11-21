332e9bd3d99e62d3628cfab808c0dfe40295f2b9
fbierth <felicitasbierth@gmail.com>
2024-11-21 12:49:29 +0100

    FF FR optimization plot

--- FR_FR_opt_plot.py
+++ FR_FR_opt_plot.py
@@ -1,5 +1,5 @@
-from Simulation_final import simulate_model
 import optuna
+from FF_Sim import simulate_model, plot_simulated_and_experimental_data
 import matplotlib.pyplot as plt
 
 
@@ -14,18 +14,36 @@ sampler = optuna.samplers.TPESampler()
 study = optuna.create_study(study_name= "GP_24_FR_FF", storage= storage_url, load_if_exists = True,
                             directions = ['minimize', 'minimize'], sampler = sampler)
 
-stimuli = [trial.params[f'stimulus{j + 1}'] for j in range(num_stimuli)]
+best_trial = study.best_trials[0]
+best_params = best_trial.params
+
+# Retrieve the optimal stimulus values
+stimuli = [best_params[f'stimulus{i + 1}'] for i in range(num_stimuli)]
+
 
 # Run the simulation with the optimal stimuli
-penalty_ff, penalty_rates, sim_ff, sim_rates, time_axis_ff, time_axis_rates, _, _, _ = simulate_model(
+penalty_ff, penalty_rates, fano_factors, firing_rates, time_axis_ff, time_axis_rates, exp_time_ff, exp_ff, exp_time_rates, exp_rates = simulate_model(
     experimental_trials=trials,
     direction_range=direction_range,
     stim_kernel=stimuli,
     kernel_step=kernel_step,
+    plot=False,
     use_delta=True,
-    plot=False
 )
 
+plot_simulated_and_experimental_data(
+    simulated_ff=fano_factors,
+    simulated_rates=firing_rates,
+    time_axis_ff=time_axis_ff,
+    time_axis_rates=time_axis_rates,
+    exp_time_ff=exp_time_ff,
+    exp_ff=exp_ff,
+    exp_time_rates=exp_time_rates,
+    exp_rates=exp_rates,
+    stim_kernel=stimuli,
+    kernel_step=kernel_step,
+    plot_delta=True  # Zeigt auch die Delta-Werte
+)
 
 
 
