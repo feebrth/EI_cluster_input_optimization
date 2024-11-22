225a374b9b5df692e8df86cc809125ffc6410ff4
fbierth <felicitasbierth@gmail.com>
2024-11-22 10:33:31 +0100

    optuna plot

--- FR_FR_opt_plot.py
+++ FR_FR_opt_plot.py
@@ -2,24 +2,36 @@ import optuna
 from FF_Sim import simulate_model, plot_simulated_and_experimental_data
 import matplotlib.pyplot as plt
 
-
 # Parameters for the Optuna study and simulation
 trials = 60
 direction_range = [0, 1, 2]
 num_stimuli = 24
 kernel_step = 2000 // num_stimuli  # 167 ms pro Stimulus
 
+# Optuna Study laden
 storage_url = "mysql://optuna:password@127.0.0.1:3306/optuna_db"
-sampler = optuna.samplers.TPESampler()
-study = optuna.create_study(study_name= "GP_24_FR_FF", storage= storage_url, load_if_exists = True,
-                            directions = ['minimize', 'minimize'], sampler = sampler)
+study = optuna.load_study(study_name="GP_24_FR_FF", storage=storage_url)
 
-best_trial = study.best_trials[0]
-best_params = best_trial.params
+# Pareto-Front visualisieren
+optuna.visualization.plot_pareto_front(study, target_names=["Penalty FF", "Penalty Rates"])
+plt.title("Pareto Front for Penalties")
+plt.savefig("Pareto_Front.png")
+
+# Optional: Alle Pareto-Lösungen anzeigen
+print(f"Number of Pareto solutions: {len(study.best_trials)}")
+for i, trial in enumerate(study.best_trials):
+    print(f"Pareto solution {i}:")
+    print("  Penalty FF:", trial.values[0])
+    print("  Penalty Rates:", trial.values[1])
+    print("  Stimulus Parameters:", trial.params)
+
+# Wähle einen spezifischen Trial aus der Pareto-Front (z. B. den ersten)
+selected_trial = study.best_trials[0]  # Alternativ: Lass den Benutzer einen Trial auswählen
+best_params = selected_trial.params
+print(f"Selected Trial Penalties: {selected_trial.values}")
 
 # Retrieve the optimal stimulus values
 stimuli = [best_params[f'stimulus{i + 1}'] for i in range(num_stimuli)]
-
 
 # Run the simulation with the optimal stimuli
 penalty_ff, penalty_rates, fano_factors, firing_rates, time_axis_ff, time_axis_rates, exp_time_ff, exp_ff, exp_time_rates, exp_rates = simulate_model(
@@ -31,6 +43,7 @@ penalty_ff, penalty_rates, fano_factors, firing_rates, time_axis_ff, time_axis_r
     use_delta=True,
 )
 
+# Plot the results with the optimal stimuli
 plot_simulated_and_experimental_data(
     simulated_ff=fano_factors,
     simulated_rates=firing_rates,
@@ -44,14 +57,4 @@ plot_simulated_and_experimental_data(
     kernel_step=kernel_step,
     plot_delta=True  # Zeigt auch die Delta-Werte
 )
-
-
-
-
-
-
-
-
-
-optuna.visualization.plot_pareto_front(study, target_names=["Penalty FF", "Penalty Rates"])
-plt.show()
+plt.savefig("Optimal_Simulation.png")
