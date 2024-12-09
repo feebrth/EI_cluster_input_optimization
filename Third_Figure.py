19690bae30ae8d4320ffb6e8763a838444aee245
fbierth <felicitasbierth@gmail.com>
2024-12-09 14:26:35 +0100

    Improvements Third Figure Schriftgröße 8 FF FR

--- Third_Figure.py
+++ Third_Figure.py
@@ -154,7 +154,7 @@ handles = [
 fig.legend(handles=handles, loc="lower center", ncol=3, fontsize=14, frameon=False)
 
 # Nummerierung der Subplot-Reihen hinzufügen
-pareto_ax.text(-0.1, 1.1, "(a)", transform=pareto_ax.transAxes, fontsize=16, fontweight="bold")
+pareto_ax.text(0.02, 1.1, "(a)", transform=pareto_ax.transAxes, fontsize=16, fontweight="bold")
 
 # Position für Stim. Amp.
 fig.text(0.02, 0.71, "(b)", fontsize=16, fontweight="bold", ha="left", va="center", rotation=0)
