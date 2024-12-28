6f3111a2bb7e3e4f59b33b738a1857ba3732b1fc
fbierth <felicitasbierth@gmail.com>
2024-12-28 15:48:15 +0100

    Third fig

--- Third_Figure.py
+++ Third_Figure.py
@@ -160,13 +160,13 @@ fig.legend(handles=handles, loc="lower center", ncol=3, fontsize=20, frameon=Fal
 
 # Nummerierung der Subplot-Reihen hinzufügen
 # Einheitliche x-Position für alle Reihen-Nummerierungen
-x_position = 0.02  # Einheitliche x-Position für alle Reihen
+x_position = 0.01  # Einheitliche x-Position für alle Reihen
 
 # Nummerierung der Subplot-Reihen mit fig.text() für globale Ausrichtung
 fig.text(x_position, 0.90, "(a)", fontsize=20, fontweight="bold", ha="left", va="center", rotation=0)
-fig.text(x_position, 0.73, "(b)", fontsize=20, fontweight="bold", ha="left", va="center", rotation=0)
-fig.text(x_position, 0.56, "(c)", fontsize=20, fontweight="bold", ha="left", va="center", rotation=0)
-fig.text(x_position, 0.31, "(d)", fontsize=20, fontweight="bold", ha="left", va="center", rotation=0)
+fig.text(x_position, 0.63, "(b)", fontsize=20, fontweight="bold", ha="left", va="center", rotation=0)
+fig.text(x_position, 0.46, "(c)", fontsize=20, fontweight="bold", ha="left", va="center", rotation=0)
+fig.text(x_position, 0.28, "(d)", fontsize=20, fontweight="bold", ha="left", va="center", rotation=0)
 
 
 
