e0b2650397aa26488da35e5326d50f610477e4b0
fbierth <felicitasbierth@gmail.com>
2025-01-04 13:13:28 +0100

    Third fig

--- Third_Figure.py
+++ Third_Figure.py
@@ -170,7 +170,7 @@ fig.text(x_position, 0.25, "(d)", fontsize=24, fontweight="bold", ha="right", va
 
 
 
-plt.tight_layout(rect=[0.10, 0.02, 0.98, 0.98])
-plt.savefig("Third_Figure_test8.png", dpi=300)
+plt.tight_layout(rect=[0.20, 0.02, 0.98, 0.98])
+plt.savefig("Third_Figure_test9.png", dpi=300)
 
 
