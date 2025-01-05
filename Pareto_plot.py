cb1949112b1fefb253c15b28107d88b15c827ed2
fbierth <felicitasbierth@gmail.com>
2025-01-05 15:25:08 +0100

    Third fig

---
+++ Pareto_plot.py
@@ -0,0 +1,19 @@
+import optuna
+from optuna.visualization import plot_pareto_front
+
+# Lade die Studie
+study = optuna.load_study(study_name="4_RS", storage="mysql://optuna:password@127.0.0.1:3306/optuna_db")
+
+# Pareto-Front-Plot generieren
+fig = plot_pareto_front(study)
+
+# Schriftgrößen und Layout anpassen
+fig.update_layout(
+    font=dict(size=18),  # Allgemeine Schriftgröße
+    xaxis_title=dict(font=dict(size=18)),  # X-Achsenbeschriftung
+    yaxis_title=dict(font=dict(size=18)),  # Y-Achsenbeschriftung
+    legend=dict(font=dict(size=16))  # Schriftgröße der Legende
+)
+
+# Speichern der Plotly-Figur
+fig.write_image("pareto_4_RS.png", width=1200, height=800)  # Angepasste Bildgröße
