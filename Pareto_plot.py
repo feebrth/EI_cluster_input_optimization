40c3e3b55b88cfd4f9aa0edc3865aa9a971ae68f
fbierth <felicitasbierth@gmail.com>
2025-01-05 15:30:52 +0100

    Third fig

--- Pareto_plot.py
+++ Pareto_plot.py
@@ -2,18 +2,34 @@ import optuna
 from optuna.visualization import plot_pareto_front
 
 # Lade die Studie
-study = optuna.load_study(study_name="GP_4", storage="mysql://optuna:password@127.0.0.1:3306/optuna_db")
+# study = optuna.load_study(study_name="GP_4", storage="mysql://optuna:password@127.0.0.1:3306/optuna_db")
+#
+# # Pareto-Front-Plot generieren
+# fig = plot_pareto_front(study)
+#
+# # Schriftgrößen und Layout anpassen
+# fig.update_layout(
+#     font=dict(size=18),  # Allgemeine Schriftgröße
+#     xaxis_title=dict(font=dict(size=18)),  # X-Achsenbeschriftung
+#     yaxis_title=dict(font=dict(size=18)),  # Y-Achsenbeschriftung
+#     legend=dict(font=dict(size=16))  # Schriftgröße der Legende
+# )
+#
+# # Speichern der Plotly-Figur
+# fig.write_image("pareto_4_GP.png", width=1200, height=800)  # Angepasste Bildgröße
 
-# Pareto-Front-Plot generieren
-fig = plot_pareto_front(study)
+import optuna
 
-# Schriftgrößen und Layout anpassen
-fig.update_layout(
-    font=dict(size=18),  # Allgemeine Schriftgröße
-    xaxis_title=dict(font=dict(size=18)),  # X-Achsenbeschriftung
-    yaxis_title=dict(font=dict(size=18)),  # Y-Achsenbeschriftung
-    legend=dict(font=dict(size=16))  # Schriftgröße der Legende
-)
+# Verbindung zur Datenbank herstellen
+storage = optuna.storages.RDBStorage("mysql://optuna:password@127.0.0.1:3306/optuna_db")
 
-# Speichern der Plotly-Figur
-fig.write_image("pareto_4_GP.png", width=1200, height=800)  # Angepasste Bildgröße
+# Alle Studien auflisten
+study_summaries = storage.get_all_study_summaries()
+
+# Studiennamen ausgeben
+if study_summaries:
+    print("Studien in der Datenbank:")
+    for summary in study_summaries:
+        print(f"- Study ID: {summary.study_id}, Name: {summary.study_name}")
+else:
+    print("Keine Studien in der Datenbank gefunden.")
