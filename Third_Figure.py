8165098a57ed245f6f60e5afc3becaa38ae4d6ac
fbierth <felicitasbierth@gmail.com>
2025-01-19 21:39:05 +0100

    FF sim

--- Third_Figure.py
+++ Third_Figure.py
@@ -50,12 +50,12 @@ grid = fig.add_gridspec(5, 5, left=0.10, right= 0.95, top=0.98, bottom=0.15, hei
 # Pareto-Front-Plot
 pareto_ax = fig.add_subplot(grid[0, :])
 pareto_ax.scatter(loss_ff, loss_rates, color="blue", label='Trials', alpha=0.9, s= 70)
-pareto_ax.set_xlabel("Loss FF", fontsize=30)
-pareto_ax.set_ylabel("Loss Rates", fontsize=30)
+pareto_ax.set_xlabel("Loss FF", fontsize=34)
+pareto_ax.set_ylabel("Loss Rates", fontsize=34)
 #pareto_ax.set_title("Pareto-Front")
 pareto_ax.grid(alpha=0.3)
 # Erstelle die Legende
-pareto_ax.legend(fontsize=28)  # Passe die Schriftgröße hier an
+pareto_ax.legend(fontsize=24)  # Passe die Schriftgröße hier an
 
 
 # Markiere die ausgewählten Trials und nummeriere sie
